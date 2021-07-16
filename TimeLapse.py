dc5f6f9873f5cad97ff4b917b15ed7b330692a54
Christian Schlaiss <christian.schlaiss@rwth-aachen.de>
2021-07-16 13:22:44 +0200

    Added function to detect time of last image so that at startup the timelapse is continued

--- TimeLapse.py
+++ TimeLapse.py
@@ -35,20 +35,7 @@ def setup():
     utils.write_to_log("Done.")
 
     #Runtime config
-    cur_time = cur_time = datetime.now().time()
-    if os.path.exists(params["save_path"]):
-        pictures = glob.glob(os.path.join(params["save_path"],'*.{}'.format("jpg")))
-        print(cur_time)
-        print(pictures[-1])
-        temp = pictures[-1][-13:-6].split("-")
-        print(temp)
-        temp = time(int(temp[0]), int(temp[1]), int(temp[2]))
-        if cur_time.time()-temp.time():
-            print(True)
-            print(cur_time.time()-temp.time())
-        
-    else:
-        cur_time = datetime.now().time()
+    cur_time = datetime.now().time()
     cur_weekday = date.today().weekday()
     
 
