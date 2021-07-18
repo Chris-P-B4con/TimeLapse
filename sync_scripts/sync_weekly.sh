845943d39f14edd93defeb6f50321948b618799f
Chris-P-B4con <chris.schlaiss@gmail.com>
2021-07-18 13:08:34 +0200

    Fixed logic in weekly sync script to only allow deletion after succesful upload

--- sync_scripts/sync_weekly.sh
+++ sync_scripts/sync_weekly.sh
@@ -3,13 +3,14 @@
 date '+%d-%b-%Y %H:%M:%S: Checking free storage...' >> log.txt
 df -h | grep /dev/root
 
-success = 1
+
 date '+%d-%b-%Y %H:%M:%S: Uploading files to OneDrive...' >> log.txt
 
-rclone copy -v /home/pi/TimeLapse/Manual $1/Manual --log-file=log.txt
-rclone copy -v /home/pi/TimeLapse/Pictures/ $1/Pictures  --log-file=log.txt 
-if [$? -eq 0] || [$? -eq 9]
-then
+rclone copy -vv /home/pi/TimeLapse/Manual $1/Manual --log-file=log.txt
+rclone copy -vv /home/pi/TimeLapse/Pictures/ $1/Pictures  --log-file=log.txt
+
+suc=$?
+if [ "$suc" -eq 0 ] || [ "$suc" -eq 9 ]; then
 	date '+%d-%b-%Y %H:%M:%S: Deleting local pictures...' >> log.txt
 	rm Pictures/*.jpg
 	date '+%d-%b-%Y %H:%M:%S: Done.' >> log.txt
