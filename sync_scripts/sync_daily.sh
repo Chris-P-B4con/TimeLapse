70a688aab57f990013294712c4a93b0e204ae938
Christian Schlaiss <christian.schlaiss@rwth-aachen.de>
2021-07-18 13:42:08 +0200

    Minor bug fixes.

--- sync_scripts/sync_daily.sh
+++ sync_scripts/sync_daily.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-echo ""
+echo "\n"
 echo "++++++++++++++++++++ END OF DAY ++++++++++++++++++++" >> log.txt
 date '+%d-%b-%Y %H:%M:%S: Uploading files to OneDrive...' >> log.txt
-rclone copy -v /home/pi/TimeLapse/Pictures $1/Pictures  --log-file=log.txt
 rclone copy -v /home/pi/TimeLapse/Manual $1/Manual --log-file=log.txt
+rclone copy -v /home/pi/TimeLapse/Pictures $1/Pictures  --log-file=log.txt
+
+if [$? -ne 0] || [$? -ne 9]; then
+    date '+%d-%b-%Y %H:%M:%S: Something went wrong during upload.' >> log.txt
+fi
 
 date '+%d-%b-%Y %H:%M:%S: Updating local config files...' >> log.txt
 rclone copy $1/config.txt /home/pi/TimeLapse/
-date '+%d-%b-%Y %H:%M:%S: Done' >> log.txt
+if [ $? -eq 0] || [ $? -eq 9]; then
+    date '+%d-%b-%Y %H:%M:%S: Done' >> log.txt
+else 
+    date '+%d-%b-%Y %H:%M:%S: Failed to update config.' >> log.txt
+fi
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++" >> log.txt
-echo ""
+echo "\n"
 
 rclone copy /home/pi/TimeLapse/log.txt $1
 rclone copy /home/pi/TimeLapse/error.txt $1
